b2fcf60947aa5db7afd018c29d44d97f5d47c858
odaysleft <vincentarasa254@gmail.com>
2025-04-16 21:06:08 +0300

    changed the name of the button and added a class to see if it can solve my problem

--- quiz.js
+++ quiz.js
@@ -171,7 +171,7 @@ const a_text = document.getElementById("a_text")
 const b_text = document.getElementById("b_text")
 const c_text = document.getElementById("c_text")
 const d_text = document.getElementById("d_text")
-const submitBtn = document.querySelector('button');
+const submitBtn = document.querySelector('.quiz-button');
 let quizNumber = document.getElementById('quizNumber');
 let score = 0;
 
