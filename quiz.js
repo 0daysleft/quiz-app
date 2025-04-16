49e2edc82d429e93cc1e2b31441cf2ac62c811ce
odaysleft <vincentarasa254@gmail.com>
2025-04-16 11:17:37 +0300

    initialized an object for storing all the questions and answers

---
+++ quiz.js
@@ -0,0 +1,18 @@
+const quizData = [
+     {
+          question: "What year was javascript invented?",
+          a_1: "1980",
+          a_2: "1980",
+          a_3: "1980",
+          a_4: "1980",
+          correct: 'c'
+     },
+     {
+          question: "What year was javascript invented?",
+          a_1: "1980",
+          a_2: "1980",
+          a_3: "1980",
+          a_4: "1980",
+          correct: 'c'
+     },
+]
