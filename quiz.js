8756600bd36dd7e1227cdfc6dc488581889e5633
odaysleft <vincentarasa254@gmail.com>
2025-04-16 11:59:43 +0300

    added a button eventlistener to change to the next question

--- quiz.js
+++ quiz.js
@@ -166,10 +166,10 @@ const a_text = document.getElementById("a_text")
 const b_text = document.getElementById("b_text")
 const c_text = document.getElementById("c_text")
 const d_text = document.getElementById("d_text")
+const submitBtn = document.querySelector('button');
 
 let currentQuestion = 0;
 
-loadQuiz();
 
 function loadQuiz(){
 
@@ -182,7 +182,14 @@ function loadQuiz(){
      c_text.innerText = currentQuizData.c;
      d_text.innerText = currentQuizData.d;
 
-     currentQuestion++;
+     
 }
 
+submitBtn.addEventListener('click', 
+     () => {
+          
+          currentQuestion++;
+          loadQuiz();
+     }
+)
 
